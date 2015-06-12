25f88238847936a0843a8ea1c6d5ab4501292140
mailmindlin <mailmindlin@users.noreply.github.com>
2015-06-11 20:29:19 -0500

    Update JSExtern.java

--- src/com/mindlin/jjsbridge/JSExtern.java
+++ src/com/mindlin/jjsbridge/JSExtern.java
@@ -1,5 +1,20 @@
 package com.mindlin.jjsbridge
 
 public @interface JSExtern {
-
+	/**
+	 * Not empty iff the name 
+	String name default "";
+	/**
+	 * {@code true} iff the type of this property descriptor may be changed and if the property may be deleted from the corresponding object.
+	 */
+	boolean configurable() default false;
+	/**
+	 * True iff this property shows up during the enumeration of the properties on the corresponding object.
+	 */
+	boolean enumerable() default false;
+	/**
+	 * True iff the value associated with the prperty may be changed with an assignment operator.
+	 * Must be false for final fields (setting other wise will either throw an error or be ignored)
+	 */
+	boolean writable() default false;
 }
